0051dc2a5621051b35772582530c672c7d12dee5
L Stupin <laura.stupin@gmail.com>
2019-03-07 13:04:09 +0100

    Check for zeros, master CDF plot

---
+++ 0data_inspect.R
@@ -0,0 +1,45 @@
+pacman::p_load(plotly, data.table, reshape2, ggplot2)
+
+dt <- fread('data/iphone_smallmatrix_labeled_8d.csv')
+
+str(dt)
+summary(dt)
+
+plot_ly(dt, x= ~df$iphonesentiment, type='histogram')
+
+
+
+#### Check for zero rows #### ---------------------------------------------
+
+# Total observations = 12973
+nrow(dt)
+
+# Number of rows that are all zero = 0
+nrow(dt[rowSums(dt) == 0])
+
+# Drop first two columns
+f0 <- dt[, -c(1:2)]
+
+# Rows with no value = 592
+nrow(dt[rowSums(f0) == 0])
+
+# Total columns = 59
+ncol(dt)
+
+# Number of columns with zero values = 0
+sum(colSums(dt) == 0)
+
+# Number of NA values = 0
+sum(is.na(dt))
+
+# Plot CDFs
+
+# Two columns with variable and value
+dt2 <- melt(dt)
+
+p <- ggplot(dt2, aes(value, color = variable)) + stat_ecdf(geom = "point")
+p
+ggplotly(p)
+
+
+#### Sandbox #### ------------------------------------------------------------
