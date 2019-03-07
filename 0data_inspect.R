e124c8fd938b13df73c3e97c6eaf5e6ce08b0481
L Stupin <laura.stupin@gmail.com>
2019-03-07 16:00:36 +0100

    MVP lm & rf models

--- 0data_inspect.R
+++ 0data_inspect.R
@@ -1,20 +1,65 @@
-pacman::p_load(plotly, data.table, reshape2, ggplot2, dplyr)
+pacman::p_load(plotly, data.table, reshape2, ggplot2, dplyr, 
+               caret, caretEnsemble, doParallel)
 
 dt_all <- fread('data/iphone_smallmatrix_labeled_8d.csv')
 
 str(dt)
 summary(dt)
 
-plot_ly(dt, x= ~df$iphonesentiment, type='histogram')
+dt <- dt_all
+
+#### Histogram target variable #### ---------------------------------------
+
+plot_ly(dt, x= ~dt$iphonesentiment, type='histogram')
+
+#### MVP Model #### -----------------------------------------------
+phone <- "iphone"
+set.seed(42)
+
+dt <- dt_all %>% dplyr:: select(starts_with(phone))
+
+tar_name <- paste0(phone, 'sentiment')
+
+rm(X)
+
+# Select all non-target columns
+# Convert to data frame, caret doesn't seem to deal well with data tables
+X <- dt %>% select(-c(tar_name)) %>% setDF()
+y <- dt %>% select(c(tar_name)) %>% setDF()
 
 
+
+part <- createDataPartition(dt[, get(tar_name)], 
+                            p = 0.75,
+                            list = FALSE)
+X_train <- X[part, ]
+X_test <- X[-part, ]
+y_train <- y[part, ]
+y_test <- y[-part, ]
+
+str(X_train)
+str(X_test)
+str(y_train)
+str(y_test)
+
+registerDoParallel(2)
+getDoParWorkers()
+
+
+control <- trainControl(method = 'cv',
+                        number = 5,
+                        savePredictions = "final",
+                        allowParallel = TRUE)
+
+model_list <- caretList(x=X_train, y=y_train,
+                        trControl = control,
+                        methodList = c('lm', 'rf'),
+                        tuneList = NULL,
+                        continue_on_fail = FALSE)
+                        # preProcess = c('center', 'scale'))
 
 #### Check for zero rows #### ---------------------------------------------
-phone <- "iphone"
 
-dtP <- dt %>% dplyr:: select(starts_with(phone))
-
-dt <- dtP
 
 # Total observations = 12973
 nrow(dt)
@@ -53,6 +98,7 @@ p <- ggplot(dt2, aes(value, color = variable)) + stat_ecdf(geom = "point")
 p
 ggplotly(p)
 
+#### Histogram Plot Target ####
 
 
 #### Sandbox #### ------------------------------------------------------------
