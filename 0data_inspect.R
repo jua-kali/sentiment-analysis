faa11eba77ee5a6d85d183bb005119ed349b59b7
L Stupin <laura.stupin@gmail.com>
2019-03-07 13:35:28 +0100

    Select variables by phone, and boxplot

--- 0data_inspect.R
+++ 0data_inspect.R
@@ -1,6 +1,6 @@
-pacman::p_load(plotly, data.table, reshape2, ggplot2)
+pacman::p_load(plotly, data.table, reshape2, ggplot2, dplyr)
 
-dt <- fread('data/iphone_smallmatrix_labeled_8d.csv')
+dt_all <- fread('data/iphone_smallmatrix_labeled_8d.csv')
 
 str(dt)
 summary(dt)
@@ -10,6 +10,11 @@ plot_ly(dt, x= ~df$iphonesentiment, type='histogram')
 
 
 #### Check for zero rows #### ---------------------------------------------
+phone <- "iphone"
+
+dtP <- dt %>% dplyr:: select(starts_with(phone))
+
+dt <- dtP
 
 # Total observations = 12973
 nrow(dt)
@@ -17,8 +22,8 @@ nrow(dt)
 # Number of rows that are all zero = 0
 nrow(dt[rowSums(dt) == 0])
 
-# Drop first two columns
-f0 <- dt[, -c(1:2)]
+# Drop the phone column
+f0 <- select(dt, -c(phone))
 
 # Rows with no value = 592
 nrow(dt[rowSums(f0) == 0])
@@ -32,14 +37,22 @@ sum(colSums(dt) == 0)
 # Number of NA values = 0
 sum(is.na(dt))
 
-# Plot CDFs
+# Max values
+colMax <- function(data) sapply(data, max, na.rm = TRUE)
+colMax(dt)
 
+#### Boxplot #### ------------------------------------------------------------
 # Two columns with variable and value
 dt2 <- melt(dt)
 
+plot_ly(dt2, x = ~variable, y = ~value, type ="box") 
+
+
+#### CDF plot #### -----------------------------------------------------------
 p <- ggplot(dt2, aes(value, color = variable)) + stat_ecdf(geom = "point")
 p
 ggplotly(p)
+
 
 
 #### Sandbox #### ------------------------------------------------------------
