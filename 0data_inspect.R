84886122ea15540528a6d55b6e885a8968eb9d10
L Stupin <laura.stupin@gmail.com>
2019-03-11 11:16:53 +0100

    RFE confusion matrix

--- 0data_inspect.R
+++ 0data_inspect.R
@@ -8,6 +8,10 @@ summary(dt_all)  #Boxplot is the visual version of this
 
 dt <- dt_all
 
+# Set target as factor for classification
+dt$iphonesentiment <- as.factor(dt$iphonesentiment)
+
+
 #### Histogram target variable #### ---------------------------------------
 
 plot_ly(dt, x= ~dt$iphonesentiment, type='histogram')
@@ -45,16 +49,74 @@ nzvMetrics
 nzv <- nearZeroVar(dt, saveMetrics = FALSE)
 nzv
 
+df <- setDF(dt)
 # Remove near zero variance variable
 iphoneNZV <- df[, -nzv]
+
+
+#### Recursive Feature Elimination ####
+rfe_df <- df
+
+# Sample data
+set.seed(13)
+iphoneSample <-rfe_df[sample(1:nrow(rfe_df), 1000, replace = FALSE), ]
+
+registerDoParallel(2)
+getDoParWorkers()
+
+# Set up rfeControl with randomforest
+ctrl <- rfeControl(functions = rfFuncs, 
+                   method = "repeatedCV",
+                   repeats = 5,
+                   verbose = FALSE,
+                   allowParallel = TRUE)
+
+
+
+vars <- length(iphoneSample)
+
+rfeResults <- rfe(iphoneSample[, 1:(vars -1)], 
+                  iphoneSample$iphonesentiment,
+                  sizes=(1:(vars-1)),
+                  rfeControl=ctrl)
+
+rfeResults
+
+# saveRDS(rfeResults, "models/rfe_full.rds")
+# test <- readRDS("models/rfe_full.rds")
+
+# Plot results
+plot(rfeResults, type=c("g", "o"))
+
+# Create a new dataset with recommended predictors
+iphoneRFE <- setDF(dt_all)[, predictors(rfeResults)]
+
+# Add dependent variable
+iphoneRFE$iphonesentiment <- dt_all$iphonesentiment
+str(iphoneRFE)
+
+# In this case, the best number is 10, but it's because 
+# I had reduced already
+
+# # create new data set with rfe recommended features
+# iphoneRFE <- iphoneDF[,predictors(rfeResults)]
+# 
+# # add the dependent variable to iphoneRFE
+# iphoneRFE$iphonesentiment <- iphoneDF$iphonesentiment
+# 
+# # review outcome
+# str(iPhoneRFE)
 
 
 
 #### Set up model variables ####
 tar_name <- paste0(phone, 'sentiment')
 
-# Convert to data frame, caret doesn't seem to deal well with data tables
-# df <- setDF(dt)
+# Choose which dataframe to work with
+# Caret doesn't seem to deal well with data tables!
+
+df <- iphoneRFE
+df$iphonesentiment <- as.factor(df$iphonesentiment)
 
 # Select all non-target columns
 X <- df %>% select(-c(tar_name)) 
@@ -74,8 +136,6 @@ str(X_test)
 str(y_train)
 str(y_test)
 
-registerDoParallel(2)
-getDoParWorkers()
 
 #### MVP Model #### -----------------------------------------------
 
@@ -89,50 +149,15 @@ model_list <- caretList(x=X_train, y=y_train,
                         trControl = control,
                         methodList = c( 'rf', 'knn'),  #'lm'
                         tuneList = NULL,
-                        continue_on_fail = FALSE)
-                        # preProcess = c('center', 'scale'))
+                        continue_on_fail = FALSE,
+                        preProcess = c('center', 'scale'))
 
 
 model_list$rf
 model_list$knn
 
-
-#### Recursive Feature Elimination ####
-
-# Sample data
-iphoneSample <-df[sample(1:nrow(df), 1000, replace = FALSE), ]
-
-# Set up rfeControl with randomforest
-ctrl <- rfeControl(functions = rfFuncs, 
-                   method = "repeatedCV",
-                   repeats = 5,
-                   verbose = FALSE,
-                   allowParallel = TRUE)
-
-names(iphoneSample)
-vars = length(iphoneSample)
-
-rfeResults <- rfe(iphoneSample[, 1:(vars -1)], 
-                  iphoneSample$iphonesentiment,
-                  sizes=(1:(vars-1)),
-                  rfeControl=ctrl)
-
-rfeResults
-
-# Plot results
-plot(rfeResults, type=c("g", "o"))
-
-# In this case, the best number is 10, but it's because 
-# I had reduced already
-
-# # create new data set with rfe recommended features
-# iphoneRFE <- iphoneDF[,predictors(rfeResults)]
-# 
-# # add the dependent variable to iphoneRFE
-# iphoneRFE$iphonesentiment <- iphoneDF$iphonesentiment
-# 
-# # review outcome
-# str(iPhoneRFE)
+pred_rf <- predict(model_list$rf, X_test)
+confusionMatrix(pred_rf, y_test)
 
 #### Check for zero rows #### ---------------------------------------------
 
